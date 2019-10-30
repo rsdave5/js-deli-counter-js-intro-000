8775667a4e829552a5fa3aed215b646624e06525
Reetu Dave <rsdave5@gmail.com>
2019-10-30 18:12:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ function nowServing(katzDeliLine) {
     katzDeliLine.shift();
     return serving;
   } else {
-    return `There is nobody waiting to be served!`
+    return `There is nobody waiting to be served!`;
   }
 }
 
 function currentLine(katzDeliLine) {
   var array = [];
   if (katzDeliLine.length === 0) {
-    return `The line is currently empty.`
+    return `The line is currently empty.`;
   } else {
     for (var i = 0; i < katzDeliLine.length; i++) {
-      array.push([i + 1] + ". " + katzDeliLine[i])
+      array.push([i + 1] + ". " + katzDeliLine[i]);
     }
-  } return "The line is currently: " + array.join(', ')
+  } return "The line is currently: " + array.join(', ');
 }
